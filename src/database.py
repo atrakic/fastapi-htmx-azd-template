4d18c389e14dd52c1a11bc621ffa75bfc6beb407
Admir Trakic <atrakic@users.noreply.github.com>
2023-09-28 12:00:46 +0200

    Bump to python 3.11

--- src/database.py
+++ src/database.py
@@ -1,9 +1,10 @@
 """Db module."""
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///db.sqlite"
+SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL", "sqlite:////tmp/db.sqlite")
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
