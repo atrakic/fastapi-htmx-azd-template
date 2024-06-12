3192e45fc0ca2c309c9ebbeca1da0961ce10d82f
atrakic <atrakic@users.noreply.github.com>
2024-06-12 16:22:40 +0200

    Update tests

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,4 +1,11 @@
+# todo: create real test cases for fastapi app
+
 class TestClass:
+    value = 0
+
     def test_one(self):
         x = "this"
         assert "h" in x
+
+    def test_two(self):
+        assert self.value != 1
